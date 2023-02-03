46f076d4a3b3a33b995c034fce0e11644f409e93
Aleksei <aleksei@mail.ru>
2023-02-03 14:44:53 +0300

    Initial commit

---
+++ Students-test-project/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  Students-test-project
+//
+//  Created by Алексей on 03.02.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+}
+
